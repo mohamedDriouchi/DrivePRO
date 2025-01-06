15140d5154f0ce8d7ec94e77818569f9481782b6
Mohamed D <mohamed.driouchi@etu.univ-amu.fr>
2025-01-06 14:14:58 +0100

    ajout du fichier javaScript

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
+  });
