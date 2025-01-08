f7f2ecaa028e8f983a5088e132f2fadebea1024a
Mohamed D <mohamed.driouchi@etu.univ-amu.fr>
2025-01-08 15:56:26 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,19 @@
-document.getElementById('theme-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme');
-    document.body.classList.toggle('light-theme');
-  });
+// Appliquer le thème
+function applyTheme(theme) {
+  document.body.classList.remove('dark-theme', 'light-theme');
+  document.body.classList.add(theme);
+}
+
+// Vérifier le thème actuel
+document.addEventListener('DOMContentLoaded', function() {
+  const storedTheme = localStorage.getItem('theme') || 'light-theme';
+  applyTheme(storedTheme);
+});
+
+// Changer le thème
+document.getElementById('theme_toggle').addEventListener('click', function() {
+  let currentTheme = document.body.classList.contains('dark-theme') ? 'dark-theme' : 'light-theme';
+  let newTheme = currentTheme === 'dark-theme' ? 'light-theme' : 'dark-theme';
+  applyTheme(newTheme);
+  localStorage.setItem('theme', newTheme);
+});
